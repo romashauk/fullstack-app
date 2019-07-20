b31873a58715d0f2c52a85bc29d6c0b5c9174aed
roma.shauk <roman.shauk@dataprobsoton.com>
2019-07-20 14:37:43 +0300

    update docker

---
+++ README.md
@@ -0,0 +1,1 @@
+# cd assets && npm install && cd .. && sudo docker-compose build && sudo docker-compose up -d
--- assets/src/index.js
+++ assets/src/index.js
@@ -3,10 +3,7 @@ import ReactDOM from 'react-dom';
 
 const render = () => {
     try {
-        ReactDOM.render(
-            <h1>Hello from app</h1>,
-            document.getElementById('root')
-        );
+        ReactDOM.render(<h1>1 2 sd s</h1>, document.getElementById('root'));
     } catch (err) {
         console.error(err);
     }
